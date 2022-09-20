329e1dbabc9042596ecb2ed7988884c9e5755b6b
toshiakiasakura <aflu.blossompaws@gmail.com>
2022-09-20 08:00:54 +0100

    Delete Japanse comments from setup.py

--- setup.py
+++ setup.py
@@ -8,23 +8,22 @@ INSTALL_REQUIRES = [
 ]
 EXTRAS_REQUIRE = {}
 
-# long_description(後述)に、GitHub用のREADME.mdを指定
 with open(path.join(current, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='myforestplot', # パッケージ名(プロジェクト名)
-    packages=['myforestplot'], # パッケージ内(プロジェクト内)のパッケージ名をリスト形式で指定
-    license='MIT', # ライセンス
+    name='myforestplot', 
+    packages=['myforestplot'], 
+    license='MIT', 
 
-    author='toshiakiasakura', # パッケージ作者の名前
-    author_email='wordpress.akitoshi@gmail.com', # パッケージ作者の連絡先メールアドレス
-    url='https://toshiakiasakura.github.io/myforestplot', # パッケージに関連するサイトのURL(GitHubなど)
+    author='toshiakiasakura', 
+    author_email='wordpress.akitoshi@gmail.com', 
+    url='https://toshiakiasakura.github.io/myforestplot', 
 
-    description='Create forestplot by Python.', # パッケージの簡単な説明
-    long_description=long_description, # PyPIに'Project description'として表示されるパッケージの説明文
-    long_description_content_type='text/markdown', # long_descriptionの形式を'text/plain', 'text/x-rst', 'text/markdown'のいずれかから指定
-    keywords='forestplot odds-ratio relative-risk meta-analysis', # PyPIでの検索用キーワードをスペース区切りで指定
+    description='A flexibly customizable Python tool to create a forest plot.', 
+    long_description=long_description, 
+    long_description_content_type='text/markdown', 
+    keywords='forestplot odds-ratio relative-risk meta-analysis', 
 
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
@@ -33,6 +32,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.9',
         'Framework :: Matplotlib',
-    ], # パッケージ(プロジェクト)の分類。https://pypi.org/classifiers/に掲載されているものを指定可能。
+    ], 
 )
 
